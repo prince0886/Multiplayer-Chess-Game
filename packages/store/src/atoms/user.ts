00dce1014f26b728a2326872b87af4a20c16f62a
Prince Kumar <prince@emvo.ai>
2026-05-17 19:09:35 +0530

    Persist user login state in localStorage to prevent logout on refresh

--- packages/store/src/atoms/user.ts
+++ packages/store/src/atoms/user.ts
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, AtomEffect } from 'recoil';
 
 export interface User {
   token: string;
@@ -6,7 +6,25 @@ export interface User {
   name: string;
 }
 
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      try {
+        setSelf(JSON.parse(savedValue));
+      } catch (e) {}
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const userAtom = atom<User | null>({
   key: 'user',
   default: null,
+  effects_UNSTABLE: [localStorageEffect<User | null>('user_session')],
 });

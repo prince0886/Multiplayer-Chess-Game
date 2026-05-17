764d7efdeca9f321efa0579e2ab92144735451ed
Prince Kumar <prince@emvo.ai>
2026-05-17 17:00:07 +0530

    Initial release of Multiplayer Chess

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,49 @@
+import dotenv from 'dotenv';
+dotenv.config();
+console.log("DB URL IS: ", process.env.DATABASE_URL);
+
+import express from 'express';
+import v1Router from './router/v1';
+import cors from 'cors';
+import { initPassport } from './passport';
+import authRoute from './router/auth';
+import session from 'express-session';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import { COOKIE_MAX_AGE } from './consts';
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET || 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: COOKIE_MAX_AGE },
+  }),
+);
+
+initPassport();
+app.use(passport.initialize());
+app.use(passport.authenticate('session'));
+
+const allowedHosts = process.env.ALLOWED_HOSTS
+  ? process.env.ALLOWED_HOSTS.split(',')
+  : [];
+
+app.use(
+  cors({
+    origin: allowedHosts,
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  }),
+);
+
+app.use('/auth', authRoute);
+app.use('/v1', v1Router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
